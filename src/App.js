603c9e13f21e25edd4931d38f0bc08d4885cb256
조기범 <akfmzh1207@naver.com>
2022-08-22 23:37:41 +0900

    라우터 가능 구현중

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import bg from "./img/bg.png";
+import { Button, Navbar, Nav, Container } from "react-bootstrap";
+import data from "./data.js";
+import { useState } from "react";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import { Detail } from "./routes/Detail.js";
 function App() {
+  let [shoes] = useState(data);
+  let navigate = useNavigate();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">eun-hak</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("./");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("./detail");
+              }}
+            >
+              Detail
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Link to="/">홈</Link>
+      <br></br>
+      <Link to="/detail">상세페이지</Link>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div>
+                <div
+                  className="main-bg"
+                  style={{ backgroundImage: "url(" + bg + ")" }}
+                ></div>
+              </div>
+              <div className="container">
+                <div className="row">
+                  {/* <Card shoes={shoes[0]} i={1}></Card>
+            <Card shoes={shoes[1]} i={2}></Card>
+            <Card shoes={shoes[2]} i={3}></Card> */}
+                  {shoes.map((a, i) => {
+                    return <Card shoes={shoes[i]} i={i}></Card>;
+                  })}
+
+                  {/* {ThreeContents.map((a) => {
+              return a;
+            })} */}
+
+                  {/* <Shoes1 />
+            <Shoes2 />
+            <Shoes3 /> */}
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route
+          path="/detail"
+          element={
+            <>
+              <Detail></Detail>
+            </>
+          }
+        />
+        <Route path="*" element={<div>없는 페이지 입니다</div>} />
+      </Routes>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
     </div>
   );
 }
